24fe7c3dc43feb5d3e9bea8f4496522efab628d5
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-04-26 15:02:28 -0400

    sei cafe completed

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,18 +1,19 @@
 import { useState } from "react";
 
-import SignUpForm from "../components/SignUpForm";
-import LoginForm from "../components/LogInForm";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from "../../components/LogInForm/LogInForm";
+import Logo from "../../components/Logo/Logo";
+import styles from "./AuthPage.module.css";
 
 function AuthPage({ setUser }) {
   const [showLogin, setShowLogin] = useState(true);
 
   return (
-    <main className="AuthPage">
-      <h1>Auth Page</h1>
-
-      <button onClick={() => setShowLogin(!showLogin)}>
+    <main className={styles.AuthPage}>
+      <Logo />
+      <h3 onClick={() => setShowLogin(!showLogin)}>
         {showLogin ? "Sign up" : "Sign in"}
-      </button>
+      </h3>
 
       {showLogin ? (
         <LoginForm setUser={setUser} />
